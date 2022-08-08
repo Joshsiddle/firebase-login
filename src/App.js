c4d8292f36c4fa03fd6026f05a57079fc154dd00
Joshua Siddle <106534417+Joshsiddle@users.noreply.github.com>
2022-08-08 18:15:02 +0100

    FireStore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,109 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { auth, db } from './firebase/init'
+import { collection, addDoc, getDocs, getDoc, doc, query, where, updateDoc, deleteDoc } from 'firebase/firestore'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, getAuthStateChanged, onAuthStateChanged } from "firebase/auth";
+
 
 function App() {
+const [user, setUser] = React.useState({});
+const [loading, setLoading] = React.useState(true);
+
+  async function updatePost() {
+    const hardcodedId = 'ajcY3IxgyaZFIkgwzyom';
+    const postRef = doc(db, 'posts', hardcodedId);
+    const post = await getPostById(hardcodedId);
+    const newPost = {
+      ...post,
+      title: 'lorem lorem'
+    };
+    updateDoc(postRef, newPost)
+  }
+
+  function deletePost() {
+    const hardcodedId = 'ajcY3IxgyaZFIkgwzyom';
+    const postRef = doc(db, 'posts', hardcodedId);
+    deleteDoc(postRef);
+  }
+
+  function createPost() {
+    const post = {
+      title: 'Finish school',
+      description: 'At 1 coding language',
+      uid: user.uid,
+    };
+    addDoc(collection(db, 'posts'), post)
+  }
+
+  async function getAllPosts() {
+    const { docs } = await getDocs(collection(db, 'posts'));
+    const posts = docs.map((elem) => ({ ...elem.data(), id: elem.id}));
+    console.log(posts);
+  }
+
+  async function getPostById(id) {
+    const postRef = doc(db, 'posts', id);
+    const postSnap = await getDoc(postRef);
+    return postSnap.data();
+  }
+
+  async function getPostByUid() {
+    const postCollectionRef = await query(
+      collection(db, 'posts'),
+      where('uid', '==', user.id)
+    );
+    const { docs } = await getDocs(collection(db, 'posts'));
+    console.log(docs.map(doc => doc.data()));
+  }
+
+  React.useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      console.log(user);
+      if (user) {
+        setUser(user)
+      }
+    })
+  }, []);
+  function register() {
+    console.log('register');
+    createUserWithEmailAndPassword(auth, 'email@email.com', 'test123')
+    .then((user) => {
+      console.log(user)
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+  function login() {
+    signInWithEmailAndPassword(auth, 'email@email.com', 'test123')
+    .then((user) => {
+      console.log(user)
+      setUser(user);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+  function logout() {
+    signOut(auth);
+    setUser({});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={register}>Register</button>
+      <button onClick={login}>Login</button>
+      <button onClick={logout}>Logout</button>
+      {loading ? 'loading...' : user.email}
+      <button onClick={createPost}>Create Post</button>
+      <button onClick={getAllPosts}>Get All Posts</button>
+      <button onClick={getPostById}>Get Post By Id</button>
+      <button onClick={getPostByUid}>Get Post By Uid</button>
+      <button onClick={updatePost}>Update Post</button>
+      <button onClick={deletePost}>Delete Post</button>
     </div>
   );
 }
